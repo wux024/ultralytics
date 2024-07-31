bce2b397ad2d63b8d9794ee51a550c57010f5831
Adam Wiveion <wux024@nenu.edu.cn>
2024-07-31 13:02:54 +0800

    update

--- tools/pose_coco_eval.py
+++ tools/pose_coco_eval.py
@@ -27,7 +27,7 @@ import numpy as np
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Validate YOLOv8 pose estimation models on a specified dataset.")
-    parser.add_argument('--model', type=str, default='yolov8n-pose', help='Path to the dataset directory.')
+    parser.add_argument('--model', type=str, default='yolov8-pose', help='Path to the dataset directory.')
     parser.add_argument('--dataset', type=str, default='ap10k', help='Name of the dataset.')
     parser.add_argument('--split', type=str, default='test', help='Split of the dataset to evaluate on.')
     return parser.parse_args()  # Return the parsed arguments
@@ -42,18 +42,26 @@ if __name__ == '__main__':
              oks_sigmas = np.array(dataset_cfg['oks_sigmas'])  # Load the OKS sigmas for pose evaluation
          else:
              oks_sigmas = np.array([1.0 / dataset_cfg['kpt_shape'][0]] * dataset_cfg['kpt_shape'][0])
-    # Load the COCO annotations and predictions
-    if args.split == 'val':
-        coco_gt = COCO(f'datasets/{args.dataset}/annotations/val.json')
-    elif args.split == 'train':
-        coco_gt = COCO(f'datasets/{args.dataset}/annotations/train.json')
-    elif args.split == 'test':
-        coco_gt = COCO(f'datasets/{args.dataset}/annotations/test.json')
+    # Set the model configuration file
+    if args.model == 'yolov8-pose':
+        models = ['yolov8n-pose', 'yolov8s-pose', 'yolov8m-pose', 'yolov8l-pose', 'yolov8x-pose']
+    elif args.model == 'yolov8-pose-cspnext':
+        models = ['yolov8n-pose-cspnext', 'yolov8s-pose-cspnext', 'yolov8m-pose-cspnext', 'yolov8l-pose-cspnext']
     else:
-        raise ValueError(f'Invalid split: {args.split}')
-    coco_dt = coco_gt.loadRes(f'runs/pose/eval/{args.dataset}/{args.dataset}-{args.model}/predictions.json')
-    # Evaluate the predictions
-    coco_eval = COCOeval(coco_gt, coco_dt, iouType='keypoints', sigmas=oks_sigmas, use_area=True)
-    coco_eval.evaluate()
-    coco_eval.accumulate()
-    coco_eval.summarize()
+        raise ValueError(f'Invalid model: {args.model}')
+    for model in models:
+        # Load the COCO annotations and predictions
+        if args.split == 'val':
+            coco_gt = COCO(f'datasets/{args.dataset}/annotations/val.json')
+        elif args.split == 'train':
+            coco_gt = COCO(f'datasets/{args.dataset}/annotations/train.json')
+        elif args.split == 'test':
+            coco_gt = COCO(f'datasets/{args.dataset}/annotations/test.json')
+        else:
+            raise ValueError(f'Invalid split: {args.split}')
+        coco_dt = coco_gt.loadRes(f'runs/pose/eval/{args.dataset}/{args.dataset}-model/predictions.json')
+        # Evaluate the predictions
+        coco_eval = COCOeval(coco_gt, coco_dt, iouType='keypoints', sigmas=oks_sigmas, use_area=True)
+        coco_eval.evaluate()
+        coco_eval.accumulate()
+        coco_eval.summarize()
