9deae13c6ad325f09f8fa4a2edb42623e9e30228
Adam Wiveion <wux024@nenu.edu.cn>
2024-10-28 18:41:49 +0800

    update

--- tools/pose_stream_inference.py
+++ tools/pose_stream_inference.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 File Name: pose_stream_inference.py
 Author: wux024
 Email: wux024@nenu.edu.cn
 Created On: 2024/6/19
-Last Modified: 2024/6/19
-Version: 1.0.
+Last Modified: 2024/10/28
+Version: 1.1.
 
 Overview:
-    Provide a concise summary of the file's functionality, objectives, or primary logic implemented.
+    Stream inference script for various pose estimation models on a specified dataset with default or user-provided settings.
 
 Notes:
     - Modifications should be documented in the "Revision History" section beneath this.
@@ -16,40 +17,130 @@ Notes:
 
 Revision History:
     - [2024/6/19] wux024: Initial file creation
+    - [2024/10/28] wux024: Added support for multiple model types and optical field parameters
 """
 
 import argparse
 import os
 import yaml
-
+import cv2
+import numpy as np
 from ultralytics import YOLO
 from ultralytics.utils.pose_cfg import SetSkeleton
 
-ANIMALRTPOSE = [
-    "animalpose-n",
-    "animalpose-s",
-    "animalpose-m",
-    "animalpose-l",
-    "animalpose-x",
-]
 
-def build_model_path(model, dataset, seed):
-    base_path = f"./runs/animalrtpose/train/{dataset}/{model}"
-    if seed is not None:
-        base_path += f"-{seed}"
-    base_path = os.path.join(base_path, "weights/best.pt")
-    return base_path
+def parse_models(models_str, model_type="animalrtpose"):
+    """Parse the comma-separated list of model codes into a list of model YAML files."""
+    
+    # Define mappings from model codes to YAML file names for each model type
+    model_mappings = {
+        "animalrtpose": {
+            "n": "animalrtpose-n.yaml",
+            "s": "animalrtpose-s.yaml",
+            "m": "animalrtpose-m.yaml",
+            "l": "animalrtpose-l.yaml",
+            "x": "animalrtpose-x.yaml"
+        },
+        "yolov8": {
+            "n": "yolov8n-pose.yaml",
+            "s": "yolov8s-pose.yaml",
+            "m": "yolov8m-pose.yaml",
+            "l": "yolov8l-pose.yaml",
+            "x": "yolov8x-pose.yaml"
+        },
+        "yolo11": {
+            "n": "yolo11n-pose.yaml",
+            "s": "yolo11s-pose.yaml",
+            "m": "yolo11m-pose.yaml",
+            "l": "yolo11l-pose.yaml",
+            "x": "yolo11x-pose.yaml"
+        },
+        "spipose": {
+            "n": "spipose-n.yaml",
+            "s": "spipose-s.yaml",
+            "m": "spipose-m.yaml",
+            "l": "spipose-l.yaml",
+            "x": "spipose-x.yaml"
+        },
+    }
+    
+    # Check if the provided model type is valid
+    if model_type not in model_mappings:
+        raise ValueError(f"Invalid model type: {model_type}. Valid types are {', '.join(model_mappings.keys())}.")
+    
+    # Get the mapping for the specific model type
+    mapping = model_mappings[model_type]
+    
+    # Split the input string into individual model codes and map them to YAML file names
+    models = [mapping[code.strip()] for code in models_str.split(",") if code.strip() in mapping]
+    
+    return models
 
-def build_save_dir(dataset, model, seed):
-    save_dir = f"runs/animalrtpose/predict/{dataset}/{model}"
+def build_paths(model, 
+                dataset, 
+                model_type, 
+                seed, 
+                optical_field_sizes=None, 
+                sub_optical_field_sizes=None, 
+                window_size=None, 
+                inverse=False, 
+                imgsz_hadamard=None):
+    common_base = model
+    base_common = f"./runs/{model_type}/train/{dataset}"
+    dir_common = f"runs/{model_type}/predict/{dataset}"
+
+    if optical_field_sizes is not None:
+        common_base += f"-{optical_field_sizes}x{optical_field_sizes}"
+    if sub_optical_field_sizes is not None:
+        common_base += f"-sub{sub_optical_field_sizes}x{sub_optical_field_sizes}"
+    if window_size is not None:
+        common_base += f"-{window_size}x{window_size}"
+    if inverse:
+        common_base += "-inverse"
+    if imgsz_hadamard is not None:
+        common_base += f"-imgsz{imgsz_hadamard}"
     if seed is not None:
-        save_dir += f"-{seed}"
-    return save_dir
+        common_base += f"-{seed}"
+    
+    model_path = os.path.join(base_common, common_base, "weights/best.pt")
+    save_path = os.path.join(dir_common, common_base)
+    
+    return model_path, save_path
+
+def save_results(result, save_dir, img, im, im_black, im_white, args):
+    """Save the results on different backgrounds."""
+    # Update original image size
+    result.orig_shape = im.shape[:2]
+
+    # Rescale keypoints
+    keypoints_rescaled = result.keypoints.xyn.clone()
+    keypoints_rescaled[:, :, 0] *= im.shape[1]
+    keypoints_rescaled[:, :, 1] *= im.shape[0]
+    keypoints_data_cloned = result.keypoints.data.clone()
+    keypoints_data_cloned[:, :, :2] = keypoints_rescaled
+    result.keypoints.data = keypoints_data_cloned
+
+    # Save results on different backgrounds
+    for bg_img, prefix in [(im, ''), (im_black, 'black_'), (im_white, 'white_')]:
+        result.save(
+            filename=f"{save_dir}/{prefix}{img}",
+            img=bg_img,
+            conf=args.show_conf,
+            line_width=args.line_width,
+            kpt_radius=args.kpt_radius,
+            kpt_line=args.kpt_line,
+            labels=args.show_labels,
+            boxes=args.show_boxes,
+            masks=args.show_masks,
+            probs=args.show_probs,
+            show=args.show
+        )
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--dataset", type=str, default="fish", help="dataset to use")
-    parser.add_argument("--model", type=str, default="animalrtpose-n", help="model to use")
+    parser.add_argument("--models", type=str, required=True, help="comma-separated list of model codes")
+    parser.add_argument("--model_type", type=str, default="animalrtpose", help="model type")
     parser.add_argument("--conf", type=float, default=0.25, help="object confidence threshold")
     parser.add_argument("--iou", type=float, default=0.7, help="IOU threshold for NMS")
     parser.add_argument("--imgsz", type=int, default=640, help="image size")
@@ -73,38 +164,42 @@ if __name__ == "__main__":
     parser.add_argument("--line_width", type=int, default=None, help="line width for boxes")
     parser.add_argument("--kpt_radius", type=int, default=5, help="keypoint radius")
     parser.add_argument("--kpt_line", action="store_true", help="draw keypoint lines")
+    parser.add_argument("--optical_field_sizes", type=int, default=None, help="optical field sizes for embedding head")
+    parser.add_argument("--sub_optical_field_sizes", type=int, default=None, help="sample rate for inference")
+    parser.add_argument("--window_size", type=int, nargs='+', default=None, help="window size for embedding head")
+    parser.add_argument("--inverse", action="store_true", help="inverse flag for embedding head")
+    parser.add_argument("--imgsz_hadamard", type=int, default=None, help="Hadamard image size")
     parser.add_argument("--seed", type=int, default=None, help="seed for inference")
     args = parser.parse_args()
 
+    # Parse the provided models
+    models = parse_models(args.models, args.model_type)
 
     data_cdg = yaml.load(open(f"configs/data/{args.dataset}.yaml"), Loader=yaml.FullLoader)
-    
-    if "skeleton" in data_cdg.keys():
-        SetSkeleton(data_cdg["skeleton"])
-    
-    if args.model in ANIMALRTPOSE:
-        models = [args.model]
-    elif args.model == "all":
-        models = ANIMALRTPOSE
-    else:
-        raise ValueError(f"Invalid model: {args.model}")
-    
 
-    for model in models:
-        model_path = build_model_path(model, args.dataset, args.seed)
-        save_dir = build_save_dir(args.dataset, model, args.seed)
+    for model_yaml in models:
+        model_name = os.path.splitext(os.path.basename(model_yaml))[0]
+        model_path, save_dir = build_paths(model_name, args.dataset, args.model_type, args.seed, args.optical_field_sizes, args.sub_optical_field_sizes, args.window_size, args.inverse, args.imgsz_hadamard)
 
         if not os.path.exists(save_dir):
             os.makedirs(save_dir)
 
         model = YOLO(model_path)
 
-        if "test" in data_cdg.keys():
-            data_path = f"datasets/{data_cdg['path']}/{data_cdg['test']}"
-        elif "val" in data_cdg.keys():
-            data_path = f"datasets/{data_cdg['path']}/{data_cdg['val']}"
-        elif "train" in data_cdg.keys():
-            data_path = f"datasets/{data_cdg['path']}/{data_cdg['train']}"
+        # Set skeleton
+        if 'skeleton' in data_cdg.keys():
+            SetSkeleton(data_cdg['skeleton'])
+
+        # Determine data paths
+        if 'test' in data_cdg.keys():
+            data_path = os.path.join("datasets", data_cdg['path'], data_cdg['test'])
+            high_data_path = os.path.join("datasets", data_cdg['path'], "images_", "test")
+        elif 'val' in data_cdg.keys():
+            data_path = os.path.join("datasets", data_cdg['path'], data_cdg['val'])
+            high_data_path = os.path.join("datasets", data_cdg['path'], "images_", "val")
+        elif 'train' in data_cdg.keys:
+            data_path = os.path.join("datasets", data_cdg['path'], data_cdg['train'])
+            high_data_path = os.path.join("datasets", data_cdg['path'], "images_", "train")
         else:
             raise ValueError(f"No test or val or train data found in {data_cdg['path']}")
 
@@ -124,20 +219,15 @@ if __name__ == "__main__":
             classes=args.classes,
             retina_masks=args.retina_masks,
             embed=args.embed,
-            stream=True,
+            stream=True
         )
 
-        for i, result in enumerate(results):
-            img_name = os.path.basename(result.path)
-            result.save(
-                filename=f"{save_dir}/{img_name}",
-                conf=args.show_conf,
-                line_width=args.line_width,
-                kpt_radius=args.kpt_radius,
-                kpt_line=args.kpt_line,
-                labels=args.show_labels,
-                boxes=args.show_boxes,
-                masks=args.show_masks,
-                probs=args.show_probs,
-                show=args.show,
-            )
+        # high_data
+        high_datas = os.listdir(high_data_path)
+
+        for result, img in zip(results, high_datas):
+            im = cv2.imread(os.path.join(high_data_path, img))
+            im_black = np.zeros((im.shape[0], im.shape[1], 3), dtype=np.uint8)
+            im_white = np.ones((im.shape[0], im.shape[1], 3), dtype=np.uint8) * 255
+
+            save_results(result, save_dir, img, im, im_black, im_white, args)
