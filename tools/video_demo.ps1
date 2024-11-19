cee68f80db23104cefe86ed45d224564624a1043
Adam Wiveion <wux024@nenu.edu.cn>
2024-11-19 16:44:09 +0800

    update

---
+++ tools/video_demo.ps1
@@ -0,0 +1,59 @@
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right.avi `
+    --model .\runs\animalrtpose\train\twoperson\animalrtpose-n\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-64x64-aliasing-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-64x64-aliasing-256-20241029\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-32x32-aliasing-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-32x32-aliasing-256-20241029\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-16x16-aliasing-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-16x16-aliasing-256-20241030\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-128x128-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-128x128-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-64x64-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-64x64-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-32x32-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-32x32-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-16x16-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-16x16-256-20241027\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-64x64-inverse-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-64x64-inverse-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-32x32-inverse-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-32x32-inverse-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
+
+python .\tools\pose_video_demo.py `
+    --source .\runs\videos\spi_videos\twoperson-right-128x128-16x16-inverse-256.avi `
+    --model .\runs\spipose\train\twoperson\spipose-n-128x128-16x16-inverse-256-20241026\weights\best.pt `
+    --imgsz 640 --dataset twoperson --kpt-line
