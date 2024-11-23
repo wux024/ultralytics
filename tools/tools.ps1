8863d8c7a910bcd78b97019082b52454b0e3f938
Adam Wiveion <wux024@nenu.edu.cn>
2024-11-23 14:05:33 +0800

    update

--- tools/tools.ps1
+++ tools/tools.ps1
@@ -1,18 +1,19 @@
 # Define an array containing all the sub-optical field sizes to test
-$subOpticalFieldSizes = @(128,64,32)
-$dataset = "twoperson"
+$subOpticalFieldSizes = @(64,32,16)
+$dataset = "horse10"
 
 # Loop through each sub-optical field size
 foreach ($size in $subOpticalFieldSizes) {
 
     # Build and run the command with the current sub-optical field size
     python tools/pose_val.py --dataset $dataset `
-        --imgsz 640 `
+        --imgsz 256 `
         --models n `
         --model-type spipose `
         --optical-field-sizes 128 `
         --sub-optical-field-size $size `
         --imgsz-hadamard 256 `
-        --seed 20241026 `
+        --save_json `
+        --seed 20241121 `
         --split test
 }
